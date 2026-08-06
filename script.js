b433dd8e5d977129dc6ab2acddbda8a3fc0900b9
ramsesdiaz-cmyk <ramses.diaz@escola.pr.gov.br>
2026-08-06 10:43:34 -0300

    ok

---
+++ script.js
@@ -0,0 +1,15 @@
+//Botões
+
+const btnContraste = document.getElementById("btn-constrate");
+const btnAumentar = document.getElementById("btn-aumentar-texto")
+const btnDiminuir = document.getElementById("btn-diminuir-texto")
+
+//Mudar tamanho da fonte
+
+let tamanhoAtualFonte = 100;
+
+//alto constraste
+
+btnContraste.addEventListner("click", ()=>{
+    document.body.classList.
+})
