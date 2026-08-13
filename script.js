a3b5909bada044fbd799744368045b38356781ef
ramsesdiaz-cmyk <ramses.diaz@escola.pr.gov.br>
2026-08-13 10:43:17 -0300

    ok

--- script.js
+++ script.js
@@ -12,4 +12,30 @@ let tamanhoAtualFonte = 100;
 
 btnContraste.addEventListner("click", ()=>{
     document.body.classList.
-})
+ 
+    //leitor de tela
+
+    const ativo = document.body.classList.contains("alto-contraste");
+    btnContraste.setAttribute("aria-pressed", ativo);
+
+});
+
+//aumentar o texto
+
+btnAumentar.addEventListener("click", () =>{
+    if (tamanhoAtualFonte < 150) {
+        tamanhoAtualFonte +=10;
+        document.documentElement.style.fontSize = `${tamanhoAtualFonte}%`
+    }       
+});
+
+//diminuir o texto
+
+btnDiminuir.addEventListener("click", () =>{
+    if (tamanhoAtualFonte > 90){
+        tamanhoAtualFonte -=10;
+        document.documentElement.style.fontSize =  ${tamanhoAtualFonte}%`
+    }
+
+    });
+
